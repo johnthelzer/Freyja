0cf5d853478e31bef5ad2a9ff89b9a7ba66e326c
johnthelzer <johnthelzer@gmail.com>
2025-03-11 11:29:01 -0500

    base Freyja examples: works on SITL!

--- autopilot_handler/apm_handler/src/mavros_translate.cpp
+++ autopilot_handler/apm_handler/src/mavros_translate.cpp
@@ -32,8 +32,8 @@
 #define ROS_NODE_NAME "mavros_translator"
 
 double THRUST_MAX = 1.0;
-double THRUST_MIN = 0.02;
-double THRUST_SCALER = 200.0;
+double THRUST_MIN = 0.1;
+double THRUST_SCALER = 50;
 
 typedef mavros_msgs::AttitudeTarget AttiTarget;
 typedef freyja_msgs::CtrlCommand::ConstPtr CtrlInput;
